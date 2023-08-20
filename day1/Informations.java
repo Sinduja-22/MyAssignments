1bdcfe98d84b75e49dba41ab8f4493050ee2d93e
Sinduja-22 <142722218+Sinduja-22@users.noreply.github.com>
2023-08-20 15:57:35 +0530

    Add files via upload
    Week1.day1

---
+++ day1/Informations.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Informations {
+	public static void main(String[] args) {
+		int choclates = 5;
+		boolean doyouWant = false;
+	String count = "Five";
+	
+	System.out.println("How many?"+choclates+  "Is it required:" +doyouWant);
+	System.out.println(6 +count);
+	System.out.println(count);
+	
+	
+		
+	}
+
+}
